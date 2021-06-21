359db70e70f1df4f735e1d2bc25c80cffbe43308
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-21 14:59:54 -0700

    Update tail.js

--- tail.js
+++ tail.js
@@ -7,6 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  let newArray = [];
+  let newArray = array.slice(0, array.length - 1);
   return newArray;
 }
+
+// tests
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const result = tail(words);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
