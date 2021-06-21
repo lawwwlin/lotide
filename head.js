2f0ec2744ce061c9d599165a95e9d36dcd82144c
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-21 14:53:02 -0700

    Update head.js

--- head.js
+++ head.js
@@ -9,9 +9,9 @@ const assertEqual = function(actual, expected) {
 const head = function(array) {
   const elem = array[0];
   return elem;
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined))
+assertEqual(head([], undefined));
 assertEqual(head([4]), 4);
