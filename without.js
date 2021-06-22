1e062e85c9f973697f83de4b35dc95bed0396fa1
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 16:59:10 -0700

    Update without.js

--- without.js
+++ without.js
@@ -20,7 +20,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArrayEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
