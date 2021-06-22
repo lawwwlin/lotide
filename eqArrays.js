a624eb6db24ae3a1deb86f13d1528245d5f6b851
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 16:26:27 -0700

    Update eqArrays.js
    attempt to compare array in for loop, might end up in infinite loop

--- eqArrays.js
+++ eqArrays.js
@@ -19,12 +19,15 @@ const eqArrays = function(arr1, arr2) {
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    console.log("for loop");
+    console.log("for loop" + arr1[i] + arr2[i]);
     if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])){
       if (arr1[i] !== arr2[i]) {
         console.log("arr1: " + arr1[i] + " arr2: " + arr2[i]);
         return false;
       }
+    } 
+    else {
+      eqArrays(arr1[i], arr2[i]);
     }
   }
 
