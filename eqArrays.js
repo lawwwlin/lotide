2b9ffe16744ca9c7b43b283a793944d9d2682c5b
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 16:01:51 -0700

    Update eqArrays.js
    added more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -30,6 +30,9 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([], []), true); // => should PASS
 assertEqual(eqArrays(["hello"], ["hello"]), true); // => should PASS
 assertEqual(eqArrays([1], [1]), true); // => should PASS
+assertEqual(eqArrays([1, "hello", 0, p[]], [1, "hello", 0, []]), true); // => should PASS
+console.log("This should be false: " + eqArrays(undefined, ["1", "2", 3])); // testing edge case 1
+console.log("This should be false: " + eqArrays("le", ["1", "2"])); // comparing string of length 2 to array of length 2 (testing edge case 2)
 console.log("This should be false: " + eqArrays(["1", "2", "3", "4"], ["1", "2", 3]));
 console.log("This should be false: " + eqArrays(["1", "2", "3"], ["1", "2", 3]));
 console.log("This should be false: " + eqArrays([1, 2, 3], [3, 2, 1]));
