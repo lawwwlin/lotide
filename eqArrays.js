9ead291d0d4a3b62d542dfa026f51f0c1dea9c2f
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 16:31:50 -0700

    Update eqArrays.js
    take out print statements

--- eqArrays.js
+++ eqArrays.js
@@ -9,25 +9,20 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    console.log("edge case 1");
     return false;
   }
 
   if (arr1.length != arr2.length) {
-    console.log("edge case 2");
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    console.log("for loop" + arr1[i] + arr2[i]);
     if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])){
       if (arr1[i] !== arr2[i]) {
-        console.log("arr1: " + arr1[i] + " arr2: " + arr2[i]);
         return false;
       }
     } 
     else {
-      console.log("before calling eqArray again" + arr1[i] + arr2[i]);
       eqArrays(arr1[i], arr2[i]);
     }
   }
@@ -36,14 +31,14 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // test cases
+console.log("This should be false: " + eqArrays(undefined, ["1", "2", 3])); // testing edge case 1
+console.log("This should be false: " + eqArrays("le", ["1", "2"])); // comparing string of length 2 to array of length 2 (testing edge case 2)
+console.log("This should be false: " + eqArrays(["1", "2", "3", "4"], ["1", "2", 3]));
+console.log("This should be false: " + eqArrays(["1", "2", "3"], ["1", "2", 3]));
+console.log("This should be false: " + eqArrays([1, 2, 3], [3, 2, 1]));
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([], []), true); // => should PASS
 assertEqual(eqArrays(["hello"], ["hello"]), true); // => should PASS
 assertEqual(eqArrays([1], [1]), true); // => should PASS
 assertEqual(eqArrays([1, "hello", 0, []], [1, "hello", 0, []]), true); // testing array as an element
 assertEqual(eqArrays([1, "hello", 0, [], [1, "hello", 3, []]], [1, "hello", 0, [], [1, "hello", 3, []]]), true); // the ultimate test for recursion 
-console.log("This should be false: " + eqArrays(undefined, ["1", "2", 3])); // testing edge case 1
-console.log("This should be false: " + eqArrays("le", ["1", "2"])); // comparing string of length 2 to array of length 2 (testing edge case 2)
-console.log("This should be false: " + eqArrays(["1", "2", "3", "4"], ["1", "2", 3]));
-console.log("This should be false: " + eqArrays(["1", "2", "3"], ["1", "2", 3]));
-console.log("This should be false: " + eqArrays([1, 2, 3], [3, 2, 1]));
