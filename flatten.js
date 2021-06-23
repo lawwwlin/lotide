bcd0841efaa2eff9e9c9c7103285b9775c3ead63
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 19:24:39 -0700

    Update flatten.js
    one more test case with empty set in the middle

--- flatten.js
+++ flatten.js
@@ -44,7 +44,9 @@ const flattenArrays = function(arr) {
 
 const flatten = function(arr) {
   let newArr = [];
-  if (arr.length <= 1) {
+  if (arr.length === 0) {
+    //newArr.push(undefined);
+  } else if (arr.length <= 1) {
     newArr = newArr.concat(arr); // add the only element of the array to newArr
   } else {
     newArr = newArr.concat(flattenArrays(arr));
@@ -65,3 +67,7 @@ console.log(result2.length, 6);
 const result3 = flatten([[], 2, [3, ["4", 5]], 6, ["7", true]]);
 asserArraysEqual(result3, ["", 2, 3, "4", 5, 6, "7", true]);
 console.log(result3.length, ["", 2, 3, "4", 5, 6, "7", true].length);
+
+const result4 = flatten([1, "2", [3, ["4", [5, []]], 6, ["7", true]]);
+asserArraysEqual(result4, [1, "2", 3, "4", 5, [], 6, "7", true]);
+console.log(result4.length, [1, "2", 3, "4", 5, [], 6, "7", true].length);
