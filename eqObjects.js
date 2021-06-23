d64ac09f5d9af4d9586352aec5d6b88cde80f956
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 15:32:56 -0700

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -43,4 +43,4 @@ const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), true); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
