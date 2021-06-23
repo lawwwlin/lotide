32abe670b3a93ae9eb9397b08250bda5230449e0
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 13:26:37 -0700

    Update countOnly.js
    only increment our count in results if the item is found in the itemsToCount object.

--- countOnly.js
+++ countOnly.js
@@ -12,11 +12,13 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) { 
+      console.log(results[item]);
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
 
