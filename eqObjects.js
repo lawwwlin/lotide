a6af401df5c5072d04df603cd694aa39c12a78d5
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 15:34:56 -0700

    Update eqObjects.js
    added 2 more test cases for testing arrays

--- eqObjects.js
+++ eqObjects.js
@@ -44,3 +44,10 @@ assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
