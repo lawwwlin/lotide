c9a5644c59f4283719ff2b529c3d466c7b6a242c
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 15:26:28 -0700

    Update eqObjects.js
    Primitives As Values (Implementation)

--- eqObjects.js
+++ eqObjects.js
@@ -9,6 +9,31 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1); // array of keys for object1
+  const keys2 = Object.keys(object2); // array of keys for object2
+  const val1 = Object.values(object1); // array of values for object1
+  const val2 = Object.values(object2); // array of values for object2
+  if (keys1.length !== keys2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < keys1.length; i++) {
+      if (val1[i] !== val2[keys2.find(val1)]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const findKeyByValue = function(obj, val) {
+  let pos = 0;
+  for (const v of Object.values(obj)) { // goes through each value of obj
+    if (v === val) {
+      return Object.keys(obj)[pos];
+    }
+    pos++;
+  }
+  return undefined;
 };
 
 
