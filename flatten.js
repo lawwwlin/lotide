6c02ffd851690370806914fccd5664866634a539
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-22 18:02:06 -0700

    Create flatten.js
    initial implementation and simple test case

---
+++ flatten.js
@@ -0,0 +1,48 @@
+const eqArrays = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } else {
+      eqArrays(arr1[i], arr2[i]);
+    }
+  }
+
+  return true;
+};
+
+const asserArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Given an array with other arrays inside, it can flatten it into a single-level array.
+
+const flatten = function(arr) {
+  let newArr = [];
+    for (let ele of arr) {
+      if (!Array.isArray(ele)) {
+        newArr.push(ele);
+      } else {
+        flatten(ele);
+      }
+    }
+  return newArr;
+};
+
+
+
+// test cases
+asserArraysEqual(flatten([1, 2, [3, 4], 5, [6]], [1, 2, 3, 4, 5, 6]));
