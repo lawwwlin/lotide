d78ed1512778fdcd65bd5962ea38b5e3c82feacd
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 15:08:32 -0700

    Create eqObjects.js
    initial commit, function definition and test cases

---
+++ eqObjects.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
+
+// tests
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), true); // => false
