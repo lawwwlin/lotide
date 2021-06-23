1fd537f06f3c207ec008d62e5e8342ada819787c
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-23 14:27:04 -0700

    Update letterPositions.js
    change how the position is stored

--- letterPositions.js
+++ letterPositions.js
@@ -32,7 +32,11 @@ const letterPositions = function(sentence) {
   const results = {};
   let counter = 0;
   for (const s of sentence) {
-    results[s].push(counter);
+    if (results[s]) {
+      results[s].push(counter);
+    } else {
+      results[s] = [counter];
+    }
     counter++;
   }
   return results;
