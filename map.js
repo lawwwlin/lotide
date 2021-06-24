f078cc3d5ce6d9e62539cdba9bda970c944733ce
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 14:36:57 -0700

    Update map.js
    added test case 3

--- map.js
+++ map.js
@@ -45,3 +45,7 @@ assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
 const results2 = map(words, word => word + 2);
 console.log(results2);
 assertArraysEqual(results2, ["ground2", "control2", "to2", "major2", "tom2"]);
+
+const results3 = map(words, word => "");
+console.log(results3);
+assertArraysEqual(results3, ["", "", "", "", ""]);
