f8f65c99b5a65fa1bac033f1f045f9a5171c1707
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 15:48:43 -0700

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -48,20 +48,20 @@ const loopPushUntil = (array, arrToPush, callback) => {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
 
 const results3 = takeUntil(data2, x => x === '');
 console.log(results3);
-assertArraysEqual(results3, ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"])
+assertArraysEqual(results3, ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]);
 
 const results4 = takeUntil(data2, x => x === 'to');
 console.log(results4);
-assertArraysEqual(results3, ["I've", "been", "to"])
+assertArraysEqual(results4, ["I've", "been", "to"]);
 
