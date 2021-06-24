076b58cbf458a02acf77425b58ad12a5503e0bc4
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 15:17:54 -0700

    Create findKey.js
    initial commit, basic implementation to see if code works

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  const key = "";
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    if (callback()) {
+      key = "found it";
+    }
+  }
+  return key;
+};
+
+
+// tests
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+assertEqual(result1, "noma");
