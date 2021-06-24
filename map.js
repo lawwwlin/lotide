a268a222c1aa3c2ae14ce8e57a13449431cd17a4
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 14:26:47 -0700

    Update map.js
    added loop

--- map.js
+++ map.js
@@ -1,9 +1,10 @@
 const map = (array, callback) => {
-  // temporary code:
-  console.log('array: ', array);
-  console.log('callback: ', callback);
-
   const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
   return results;
 }
 
