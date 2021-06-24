dbdfc48260cab82ba69d3057eaf68146cb803740
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 14:29:41 -0700

    Update map.js
    added real implementation and test case2

--- map.js
+++ map.js
@@ -1,14 +1,46 @@
+const eqArrays = function(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } else {
+      eqArrays(arr1[i], arr2[i]);
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 const map = (array, callback) => {
   const results = [];
+  const results = [];
   for (let item of array) {
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
+    results.push(callback(item));
   }
   return results;
-}
+};
 
 // tests
 const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+const result2 = map(words, );
+assertArraysEqual(result2, words);
