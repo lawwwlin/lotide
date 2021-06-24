2ce81a3165c75b950746c6b14201f6f833b290c3
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-24 16:16:27 -0700

    Update findKey.js
    added more test cases

--- findKey.js
+++ findKey.js
@@ -43,3 +43,23 @@ const result1 = findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2) // => "noma"
 assertEqual(result1, "noma");
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1) // => "noma"
+assertEqual(result2, "Blue Hill");
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4) // => "noma"
+assertEqual(result3, "");
