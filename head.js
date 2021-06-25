0173a75064b00f015feca77ce198ef424d585a30
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-25 13:19:31 -0700

    Update head.js

--- head.js
+++ head.js
@@ -16,4 +16,3 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([], undefined));
 assertEqual(head([4]), 4);
-console.log("testing github desktop");
