b6509a7e4da785aabd6ea49a218185419ede9a63
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-25 13:19:05 -0700

    re-location of the files

--- head.js
+++ head.js
@@ -16,3 +16,4 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([], undefined));
 assertEqual(head([4]), 4);
+console.log("testing github desktop");
