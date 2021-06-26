d1dfd3c8346393a1fd5ede28efdbbe5a5ced02ac
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-25 21:38:20 -0700

    Update eqObjects.js
    added test cases and ability to check for objects

--- eqObjects.js
+++ eqObjects.js
@@ -34,26 +34,35 @@ const eqArrays = function(arr1, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  let ans = true;
   const keys1 = Object.keys(object1); // array of keys for object1
   const keys2 = Object.keys(object2); // array of keys for object2
   if (keys1.length !== keys2.length) {
     return false;
   } else {
-    for (let key of keys1) {
+    for (const key of keys1) {
       const value1 = object1[key];
-      const value2 = object2[key];
-      if (Array.isArray(value1)) {
+      const value2 = object2[key]; // same key in object 2
+      if (!Array.isArray(value1) && !Array.isArray(value2)) { // value1 and value2 are not arrays
+        if (typeof value1 === "object" && typeof value2 === "object") { // if they are both objects
+          if (!eqObjects(value1, value2)) {
+            return false;
+          }
+        }else { // they are both not arrays, and both not objects
+          if (value1 !== value2) {
+            return false;
+          }
+        }
+      } else if (Array.isArray(value1) && Array.isArray(value2)) { // they are both arrays
         if (!eqArrays(value1, value2)) {
           return false;
         }
-      } else {
-        if (value1 !== value2) {
-          return false;
-        }
+      } else { // they are different type
+        return false;
       }
     }
   }
-  return true;
+  return ans;
 };
 
 // tests
@@ -77,12 +86,14 @@ assertEqual(eqObjects(ab2, ba2), true); // => true
 
 const ab3 = { a: ["1", 2, false], b: "2" };
 const ba3 = { b: "2", a: ["1", 2, true] };
-assertEqual(eqObjects(ab2, ba2), false); // => false
+assertEqual(eqObjects(ab3, ba3), false); // => false
 
 
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);  // => true
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);  // => true
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
 
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: { y: { y: 0, z: 2 }, z: 1 }, z: 1 }, b: 2 }, { a: { y: { y: { y: 0, z: 1 }, z: 1 }, z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: { y: { y: 0, z: "1" }, z: 1 }, z: 1 }, b: 2 }, { a: { y: { y: { y: 0, z: "2" }, z: 1 }, z: 1 }, b: 2 }), false);
 
