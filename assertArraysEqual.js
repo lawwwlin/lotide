cdfda0aa5778c96a0ffc34d9003036c98d805973
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-25 20:07:40 -0700

    Update assertArraysEqual.js
    updated eqArrays.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,26 @@
 const eqArrays = function(arr1, arr2) {
+  let ans = true;
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
+    ans = false;
+    return ans;
   }
 
   if (arr1.length !== arr2.length) {
-    return false;
+    ans = false;
+    return ans;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) {
+    if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) { // any of arr1[i] or arr2[i] is not an array
       if (arr1[i] !== arr2[i]) {
-        return false;
+        ans = false;
+        return ans;
       }
-    } else {
-      eqArrays(arr1[i], arr2[i]);
+    } else { // they are both arrays
+      ans = eqArrays(arr1[i], arr2[i]);
     }
   }
-
-  return true;
+  return ans;
 };
 
 const assertArraysEqual = function(actual, expected) {
@@ -31,9 +34,9 @@ const assertArraysEqual = function(actual, expected) {
 // test cases
 assertArraysEqual(undefined, ["1", "2", 3]); // testing edge case 1
 assertArraysEqual("le", ["1", "2"]); // comparing string of length 2 to array of length 2 (testing edge case 2)
-assertArraysEqual(["1", "2", "3", "4"], ["1", "2", 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3", "4"], ["1", "2", 3]); // fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([], []); // => should PASS
 assertArraysEqual(["hello"], ["hello"]); // => should PASS
