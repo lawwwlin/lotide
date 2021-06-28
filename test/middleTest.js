98fe152038b0833716460c403e0b5039c032f89f
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 14:44:36 -0700

    Update middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,32 +1,45 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-const result0 = middle([]);
-assertArraysEqual(result0, []);
-
-const result1 = middle([1]);
-assertArraysEqual(result1, []);
-
-const result2 = middle([1, 2]);
-assertArraysEqual(result2, []);
-
-const result3 = middle([1, 2, 3]);
-assertArraysEqual(result3, [2]);
-
-const result4 = middle([1, 2, 3, 4, 5]);
-assertArraysEqual(result4, [3]);
-
-const result5 = middle([1, 2, 3, 4]);
-assertArraysEqual(result5, [2, 3]);
-
-const result6 = middle([1, 2, 3, 4, 5, 6]);
-assertArraysEqual(result6, [3, 4]);
-
-const result7 = middle(["1", "2", "3", "4", "5"]);
-assertArraysEqual(result7, ["3"]);
-
-const result8 = middle(["1", "2", "3", "4", "5", "6"]);
-assertArraysEqual(result8, ["3", "4"]);
-
-const result9 = middle(["1", "2", [], "4", "5"]);
-assertArraysEqual(result9, [[]]);
+describe("", () => {
+  it ("", () => {
+    const result0 = middle([]);
+    assert.deepEqual(result0, []);
+  });
+  it ("", () => {
+    const result1 = middle([1]);
+    assert.deepEqual(result1, []);
+  });
+  it ("", () => {
+    const result2 = middle([1, 2]);
+    assert.deepEqual(result2, []);
+  });
+  it ("", () => {
+    const result3 = middle([1, 2, 3]);
+    assert.deepEqual(result3, [2]);
+  });
+  it ("", () => {
+    const result4 = middle([1, 2, 3, 4, 5]);
+    assert.deepEqual(result4, [3]);
+  });
+  it ("", () => {
+    const result5 = middle([1, 2, 3, 4]);
+    assert.deepEqual(result5, [2, 3]);
+  });
+  it ("", () => {
+    const result6 = middle([1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(result6, [3, 4]);
+  });
+  it ("", () => {
+    const result7 = middle(["1", "2", "3", "4", "5"]);
+    assert.deepEqual(result7, ["7"]);
+  });
+  it ("", () => {
+    const result8 = middle(["1", "2", "3", "4", "5", "6"]);
+    assert.deepEqual(result8, ["3", "4"]);
+  });
+  it ("", () => {
+    const result9 = middle(["1", "2", [], "4", "5"]);
+    assert.deepEqual(result9, [[]]);
+  });
+});
