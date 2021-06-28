a31dc53df5f27e6b722aade8f401de716b71382f
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 14:17:27 -0700

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Law", "Law");
-assertEqual(1, 1);
-assertEqual(3, 6);
+module.exports = assertEqual;
