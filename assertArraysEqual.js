0d8996424cc6ab95dc9519f345af77aaac30a3df
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 14:17:24 -0700

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let ans = true;
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    ans = false;
-    return ans;
-  }
-
-  if (arr1.length !== arr2.length) {
-    ans = false;
-    return ans;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) { // any of arr1[i] or arr2[i] is not an array
-      if (arr1[i] !== arr2[i]) {
-        ans = false;
-        return ans;
-      }
-    } else { // they are both arrays
-      ans = eqArrays(arr1[i], arr2[i]);
-    }
-  }
-  return ans;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -31,15 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// test cases
-assertArraysEqual(undefined, ["1", "2", 3]); // testing edge case 1
-assertArraysEqual("le", ["1", "2"]); // comparing string of length 2 to array of length 2 (testing edge case 2)
-assertArraysEqual(["1", "2", "3", "4"], ["1", "2", 3]); // fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([], []); // => should PASS
-assertArraysEqual(["hello"], ["hello"]); // => should PASS
-assertArraysEqual([1], [1]); // => should PASS
-assertArraysEqual([1, "hello", 0, []], [1, "hello", 0, []]); // testing array as an element
-assertArraysEqual([1, "hello", 0, [], [1, "hello", 3, []]], [1, "hello", 0, [], [1, "hello", 3, []]]); // the ultimate test for recursion
+module.exports = assertArraysEqual;
