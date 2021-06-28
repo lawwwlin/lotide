12ab8a4fd48da4922910400185eb4ed56815fd61
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 14:17:30 -0700

    Update head.js

--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   const elem = array[0];
   return elem;
 };
 
-// tests
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined));
-assertEqual(head([4]), 4);
+module.exports = head;
