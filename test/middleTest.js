d8e319b4477b07fbe828ad88dbc4608b79d140ed
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 19:59:49 -0700

    Update middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,43 +2,43 @@ const middle = require('../middle');
 const assert = require('chai').assert;
 
 describe("", () => {
-  it ("", () => {
+  it ("should be [], given []", () => {
     const result0 = middle([]);
     assert.deepEqual(result0, []);
   });
-  it ("", () => {
+  it ("should be [], given [1]", () => {
     const result1 = middle([1]);
     assert.deepEqual(result1, []);
   });
-  it ("", () => {
+  it ("should be [], given [1, 2]", () => {
     const result2 = middle([1, 2]);
     assert.deepEqual(result2, []);
   });
-  it ("", () => {
+  it ("should be [2], given [1, 2, 3]", () => {
     const result3 = middle([1, 2, 3]);
     assert.deepEqual(result3, [2]);
   });
-  it ("", () => {
+  it ("should be [3], given [1, 2, 3, 4, 5]", () => {
     const result4 = middle([1, 2, 3, 4, 5]);
     assert.deepEqual(result4, [3]);
   });
-  it ("", () => {
+  it ("should be [2, 3], given [1, 2, 3, 4]", () => {
     const result5 = middle([1, 2, 3, 4]);
     assert.deepEqual(result5, [2, 3]);
   });
-  it ("", () => {
+  it ("should be [3, 4], given [1, 2, 3, 4, 5, 6]", () => {
     const result6 = middle([1, 2, 3, 4, 5, 6]);
     assert.deepEqual(result6, [3, 4]);
   });
-  it ("", () => {
+  it ("should be ['3'], given ['1', '2', '3', '4', '5']", () => {
     const result7 = middle(["1", "2", "3", "4", "5"]);
     assert.deepEqual(result7, ["3"]);
   });
-  it ("", () => {
+  it ("should be ['3', '4', given ['1', '2', '3', '4', '5', '6']", () => {
     const result8 = middle(["1", "2", "3", "4", "5", "6"]);
     assert.deepEqual(result8, ["3", "4"]);
   });
-  it ("", () => {
+  it ('should be [[]], given ["1", "2", [], "4", "5"]', () => {
     const result9 = middle(["1", "2", [], "4", "5"]);
     assert.deepEqual(result9, [[]]);
   });
