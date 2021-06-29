1f7e5737b981ab46afcd4e73a035ac5838c16b82
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-06-28 21:20:40 -0700

    Update eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -62,10 +62,3 @@ describe('eqArray', () => {
 });
 
 // object tests: stil can't compare objects
-// const person1 = {name: "Alice", age: 20};
-// const person2 = {name: "Bob", age: 20};
-// const person3 = {name: "Alice", age: 21};
-// const array5 = [person1, person2];
-// const array6 = [person1, person3];
-// assertEqual(eqArrays(array5, array6), false);
-// assertEqual(eqArrays(array5, [{name: "Alice", age: 20}, {name: "Bob", age: 20}]), true);
